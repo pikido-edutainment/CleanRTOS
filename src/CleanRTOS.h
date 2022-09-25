5293856dd07c49945487a6ae197db891d0f2ed22
Marius Versteegen <pikido.edutainment@gmail.com>
2022-09-25 21:29:37 +0200

    properties update

---
+++ src/CleanRTOS.h
@@ -0,0 +1,6 @@
+// by Marius Versteegen, 2022
+
+// This file without crt_underscore is used to comply with the Arduino IDE
+// library standards to provide a file with the same name as the library itself.
+
+#include "crt_CleanRTOS.h"
